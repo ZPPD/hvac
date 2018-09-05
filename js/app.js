d5eda453cf1e9f711c6d9b91159f92e2fb90c7e9
ZPPD <zhana.p.d@gmail.com>
2018-09-05 15:12:22 -0400

    added testemonials, modified the rest of the files and images

--- js/app.js
+++ js/app.js
@@ -49,6 +49,9 @@ if(first === '' || last === '' || email === '' || phone === '' || address === ''
 e.preventDefault();
 });
 
+//phone validation
+//pattern="((\+\d{1,3}(-|.| )?\(?\d\)?(-| |.)?\d{1,5})|(\(?\d{2,6}\)?))(-|.| )?(\d{3,4})(-|.| )?(\d{4})(( x| ext)\d{1,5}){0,1}$"
+
 /* in products make buttons and switch between commercial and residential */
 
 // Google Maps For Contacts page 
